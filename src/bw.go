ca56be46f35f4e9989494006ebdd97ef4393f078
glowdan <gao.yongzhi@immomo.com>
2017-12-17 22:23:01 +0800

    add some logic

--- src/bw.go
+++ src/bw.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"encoding/json"
 	"fmt"
+	"github.com/garyburd/redigo/redis"
 )
 
 type ServerMsg struct {
@@ -28,18 +29,31 @@ type PlayData struct {
 	Role int `json:"role"`
 }
 
-var clientMsg = make(chan ClientMsg)
+type Receiver struct {
+	ClientMsg ClientMsg
+	ws        websocket.Conn
+}
+
+var clientMsg = make(chan Receiver)
 var clients = make(map[*websocket.Conn]bool)
 var isBlack = true
+var currentPlayer = 1
 
 func main() {
+
+	redisClient, err := redis.DialURL("redis://127.0.0.1:6379")
+	if err != nil {
+		log.Fatal("Redis gone away");
+	}
+	redisClient.Do("SET", "go", "1.9.2")
+
 	fs := http.FileServer(http.Dir("../game"))
 	http.Handle("/", fs)
 
 	http.HandleFunc("/ws", handleWebSocket)
 	go handleClientMsg()
 	log.Println("http server started on :8001")
-	err := http.ListenAndServe(":8001", nil)
+	err = http.ListenAndServe(":8001", nil)
 	if err != nil {
 		log.Fatal("ListenAndServer:", err)
 	}
@@ -65,21 +79,40 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 			delete(clients, ws)
 			break
 		}
-		clientMsg <- receiver
+		res := Receiver{ClientMsg: receiver, ws: *ws}
+		clientMsg <- res
 	}
 }
 
 func handleClientMsg() {
 	for {
 		middle := <-clientMsg
-		output := logic(middle)
-		for client := range clients {
-			err := client.WriteJSON(output)
-			if err != nil {
-				log.Printf("error %v", err)
-				client.Close()
-				delete(clients, client)
-			}
+		output := logic(middle.ClientMsg)
+
+		actionSend(middle.ClientMsg, output, middle.ws)
+	}
+}
+
+func actionSend(tmp ClientMsg, output ServerMsg, ws websocket.Conn) {
+	switch tmp.Action {
+	case "init":
+		initAction(output, ws)
+	case "play":
+		playAction(output)
+	}
+
+}
+
+func initAction(output ServerMsg, ws websocket.Conn) {
+	ws.WriteJSON(output)
+}
+func playAction(output ServerMsg) {
+	for client := range clients {
+		err := client.WriteJSON(output)
+		if err != nil {
+			log.Printf("error %v", err)
+			client.Close()
+			delete(clients, client)
 		}
 	}
 }
